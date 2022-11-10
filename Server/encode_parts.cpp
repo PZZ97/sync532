f2c164d0d663af30cdae38a3ab65d561f4b91528
PZZ97 <13573227027@163.com>
2022-11-10 16:44:14 -0500

    Update encode_parts.cpp

--- Server/encode_parts.cpp
+++ Server/encode_parts.cpp
@@ -284,7 +284,7 @@ uint8_t encode(uint8_t * output_buf, uint8_t* input_buf, int inlength, int * out
                 memcpy(&output_buf[*outlength],u.arr,4);
                 (*outlength)+=4;
                 cout <<"LZWheader ="<< u.header <<"\t"<<"arr[0]="<<u.arr[0]<<"\tarr[3]="<<u.arr[3]<< endl;
-                printf("")
+              
                 memcpy(&output_buf[*outlength],output_code,outlen);
                 *outlength+=outlen;
             }
