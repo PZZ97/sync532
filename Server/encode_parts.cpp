70b0cc7278c1ea662b7085f656f0c798af6191cf
PZZ97 <13573227027@163.com>
2022-11-16 18:29:56 -0500

    update LZW

--- Server/encode_parts.cpp
+++ Server/encode_parts.cpp
@@ -184,101 +184,109 @@ void LZW(int chunk_start,int chunk_end,string &s1,int packet_size,unsigned char*
     }
     string p = "", c = "";
     p += s1[chunk_start];
-    int code = 256;
+    unsigned int code = 256;
 
     *outlen=0;
     int appeartimes=0;
     int bitsize=0;
     for (int i = chunk_start; i <chunk_end; i++) {
-        // printf("\n%c,",s1[i]);
         if (i != chunk_end - 1)
             c += s1[i + 1];
         if (table.find(p + c) != table.end()) {
-            // printf(",i=%d",i);
-            // printf("str[i]=%c",s1[i]);
             p = p + c;
         }
         else {
+            printf("\ntable[p]=%d,",table[p]);
             if(appeartimes==0){
-                 output_code[(*outlen)++] |= table[p]>>5;
-                 output_code[(*outlen)++] |= table[p] <<3;
+                output_code[(*outlen)++] |=( table[p]  &0b1111111100000)>>5;
+                output_code[(*outlen)] |= (table[p]    &0b0000000011111)<<3;
             }
             else if(appeartimes==1){
-                output_code[(*outlen)++]|=table[p]>>2;
-                output_code[(*outlen)]|=table[p] <<6;
+                output_code[(*outlen)++]|=(table[p]  &0b1110000000000)>>10;
+                output_code[(*outlen)++]|=(table[p]  &0b0001111111100)>>2;
+                output_code[(*outlen)]|=(table[p]    &0b0000000000011)<<6;
             }
             else if(appeartimes==2){
-                output_code[(*outlen)++]|=table[p]>>7;
-                output_code[(*outlen)++]|=table[p]<<1;
+                output_code[(*outlen)++]|=(table[p]  &0b1111110000000)>>7;
+                output_code[(*outlen)]|=(table[p]    &0b0000001111111)<<1;
             }
             else if(appeartimes==3){
-                output_code[(*outlen)++]|=table[p]>>4;
-                output_code[(*outlen)++]|=table[p]<<4;
+                output_code[(*outlen)++]|=(table[p]  &0b1000000000000)>>12;
+                output_code[(*outlen)++]|=(table[p]  &0b0111111110000)>>4;
+                output_code[(*outlen)]|=(table[p]    &0b0000000001111)<<4;
             }
             else if(appeartimes==4){
-                output_code[(*outlen)++]|=table[p]>>1;
-                output_code[(*outlen)]|=table[p]<<7;
+                output_code[(*outlen)++]|=(table[p]  &0b1111000000000)>>9;
+                output_code[(*outlen)++]|=(table[p]  &0b0000111111110)>>1;
+                output_code[(*outlen)]|=(table[p]    &0b0000000000001)<<7;
             }
             else if(appeartimes==5){
-                output_code[(*outlen)++]|=table[p]>>6;
-                output_code[(*outlen)++]|=table[p]<<2;
+                output_code[(*outlen)++]|=(table[p]  &0b1111111000000)>>6;
+                output_code[(*outlen)]|=(table[p]  &0b0000000111111)<<2;
             }
             else if(appeartimes==6){
-                output_code[(*outlen)++]|=table[p]>>3;
-                output_code[(*outlen)++]|=table[p]<<5;
+                output_code[(*outlen)++]|=(table[p]  &0b1100000000000)>>11;
+                output_code[(*outlen)++]|=(table[p]  &0b0011111111000)>>3;
+                output_code[(*outlen)]|=(table[p]    &0b0000000000111)<<5;
             }                
             else if(appeartimes==7){
-                output_code[(*outlen)++]|=table[p];
+                output_code[(*outlen)++]|=(table[p]    &0b1111100000000)>>8;
+                output_code[(*outlen)++]|=(table[p]    &0b0000011111111);
             }           
+            appeartimes++;
+            appeartimes%=8;
+            
             table[p + c] = code;
             code++;
             p = c;
         }
         c = "";
-        appeartimes++;
-        // if(appeartimes==8)
-        //  printf(" loop");
-        appeartimes%=8;
     }
-            if(appeartimes==0){
-                output_code[(*outlen)++] |= table[p]>>5;
-                output_code[(*outlen)++] |= table[p] <<3;
-            }
-            else if(appeartimes==1){
-                output_code[(*outlen)++]|=table[p]>>2;
-                output_code[(*outlen)]|=table[p] <<6;
-            }
-            else if(appeartimes==2){
-                output_code[(*outlen)++]|=table[p]>>7;
-                output_code[(*outlen)++]|=table[p]<<1;
-            }
-            else if(appeartimes==3){
-                output_code[(*outlen)++]|=table[p]>>4;
-                output_code[(*outlen)++]|=table[p]<<4;
-            }
-            else if(appeartimes==4){
-                output_code[(*outlen)++]|=table[p]>>1;
-                output_code[(*outlen)]|=table[p]<<7;
-            }
-            else if(appeartimes==5){
-                output_code[(*outlen)++]|=table[p]>>6;
-                output_code[(*outlen)++]|=table[p]<<2;
-            }
-            else if(appeartimes==6){
-                output_code[(*outlen)++]|=table[p]>>3;
-                output_code[(*outlen)++]|=table[p]<<5;
-            }                
-            else if(appeartimes==7){
-                output_code[(*outlen)++]|=table[p];
-            }   
+    if(appeartimes==0){
+        output_code[(*outlen)++] |=( table[p]  &0b1111111100000)>>5;
+        output_code[(*outlen)] |= (table[p]    &0b0000000011111)<<3;
+    }
+    else if(appeartimes==1){
+        output_code[(*outlen)++]|=(table[p]  &0b1110000000000)>>10;
+        output_code[(*outlen)++]|=(table[p]  &0b0001111111100)>>2;
+        output_code[(*outlen)]|=(table[p]    &0b0000000000011)<<6;
+    }
+    else if(appeartimes==2){
+        output_code[(*outlen)++]|=(table[p]  &0b1111110000000)>>7;
+        output_code[(*outlen)]|=(table[p]    &0b0000001111111)<<1;
+    }
+    else if(appeartimes==3){       
+        output_code[(*outlen)++]|=(table[p]  &0b1000000000000)>>12;
+        output_code[(*outlen)++]|=(table[p]  &0b0111111110000)>>4;
+        output_code[(*outlen)]|=(table[p]    &0b0000000001111)<<4;
+    }
+    else if(appeartimes==4){
+        output_code[(*outlen)++]|=(table[p]  &0b1111000000000)>>9;
+        output_code[(*outlen)++]|=(table[p]  &0b0000111111110)>>1;
+        output_code[(*outlen)]|=(table[p]    &0b0000000000001)<<7;
+    }
+    else if(appeartimes==5){
+        output_code[(*outlen)++]|=(table[p]  &0b1111111000000)>>6;
+        output_code[(*outlen)]|=(table[p]  &0b0000000111111)<<2;
+    }
+    else if(appeartimes==6){
+        output_code[(*outlen)++]|=(table[p]  &0b1100000000000)>>11;
+        output_code[(*outlen)++]|=(table[p]  &0b0011111111000)>>3;
+        output_code[(*outlen)]|=(table[p]    &0b0000000000111)<<5;
+    }                
+    else if(appeartimes==7){
+        output_code[(*outlen)++]|=(table[p]    &0b1111100000000)>>8;
+        output_code[(*outlen)++]|=(table[p]    &0b0000011111111);
+    }    
 
-    // (*outlen)=(*outlen)+bitsize/8+1;
     while((*outlen)%8!=0){  // padding
-        // cout<<"\npadding"<<endl;
+        cout<<"\npadding"<<endl;
         output_code[(*outlen)++]=0;
     }
 
+
 }
+
 /* &file[offset]->output_buf */
 
 uint8_t encode(uint8_t * output_buf, uint8_t* input_buf, int inlength, int * outlength ){
