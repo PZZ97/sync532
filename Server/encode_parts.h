891ab3d1d1141f5edbacb12ff488580ddd493176
PZZ97 <13573227027@163.com>
2022-11-17 18:18:29 -0500

    Update encode_parts.h

--- Server/encode_parts.h
+++ Server/encode_parts.h
@@ -44,7 +44,7 @@ void cdc(unsigned char* buff, unsigned int buff_size, IDXQ& chunk_index);
 */
 
 // void SHA_384_HW(CHUNK_pos_t begin,CHUNK_pos_t end, unsigned char* packet, unsigned int packet_size, HASH& hash_value);
-void SHA_HW( uint8_t* message,CHUNK_pos_t  chunk_start,CHUNK_pos_t chunk_end,  HASH *digest_hash){
+void SHA_HW( uint8_t* message,CHUNK_pos_t  chunk_start,CHUNK_pos_t chunk_end,  HASH *digest_hash);
 
 
 // deduplication
