5d9809a8cbf583f565b2ad66cb64563c31bb1e7a
PZZ97 <13573227027@163.com>
2022-11-17 17:42:10 -0500

    Update encoder.h

--- Server/encoder.h
+++ Server/encoder.h
@@ -4,5 +4,6 @@
 // max number of elements we can get from ethernet
 #define NUM_ELEMENTS 16384
 #define HEADER 2
-void LZW(int chunk_start,int chunk_end,string &s1,int packet_size,unsigned char*output_code,size_t * outlen);
+#include<string>
+void LZW(int chunk_start,int chunk_end,std::string &s1,int packet_size,unsigned char*output_code,size_t * outlen);
 #endif
