09ebc9f8c1f57ab91efb1ad58f8906f31deba492
PZZ97 <13573227027@163.com>
2022-11-17 10:27:02 -0500

    string work version(no printf)

--- Server/encode_parts.cpp
+++ Server/encode_parts.cpp
@@ -13,8 +13,6 @@ using namespace std;
 
 uint64_t hash_func(unsigned char* input, unsigned int pos)
 {
-//	unordered_map <int,int> uma2;
-	// put your hash function implementation here
 	uint64_t hash = 0;
 	for (int i = 0; i < WIN_SIZE; i++) {
 		hash += int(input[pos + WIN_SIZE - 1 - i]) * pow(PRIME, i + 1);
@@ -50,22 +48,6 @@ void cdc(unsigned char* buff, unsigned int buff_size, IDXQ& chunk_q)
 
 
 }
-
-// unsigned int k[64] = {
-// 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-// 	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-// 	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-// 	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-// 	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-// 	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-// 	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-// 	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-// };
-
-// uint32_t right_rot(uint32_t value, unsigned int count)
-// {
-// 	return value >> count | value << (32 - count);
-// }
 
 void SHA_HW(char* message, char*digest){
     // https://edstem.org/us/courses/27305/discussion/2053707
@@ -196,8 +178,6 @@ void LZW(int chunk_start,int chunk_end,string &s1,int packet_size,unsigned char*
         output_code[++(*outlen)]=0;
 
 }
-
-/* &file[offset]->output_buf */
 
 uint8_t encode(uint8_t * output_buf, uint8_t* input_buf, int inlength, int * outlength ){
 
