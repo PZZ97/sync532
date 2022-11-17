d1b55481c55e96a223c77cf76de9c6f241d0aaca
PZZ97 <13573227027@163.com>
2022-11-16 22:50:35 -0500

    Update encode_parts.cpp

--- Server/encode_parts.cpp
+++ Server/encode_parts.cpp
@@ -281,8 +281,8 @@ void LZW(int chunk_start,int chunk_end,string &s1,int packet_size,unsigned char*
         output_code[(*outlen)++]|=(table[p]    &0b1111100000000)>>8;
         output_code[(*outlen)]|=(table[p]    &0b0000011111111);
     }
-
-    output_code[++(*outlen)]=0;
+    if((*outlen)%13!=0)
+        output_code[++(*outlen)]=0;
     
     // while((*outlen)%13!=0){  // padding
     //     // cout<<"\npadding"<<endl;
