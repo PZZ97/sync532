462a943f32833318f5a5f6be0777bb9f14a9f783
PZZ97 <13573227027@163.com>
2022-11-17 16:28:48 -0500

    sync

--- Server/encode_parts.cpp
+++ Server/encode_parts.cpp
@@ -1,4 +1,5 @@
 #include "encode_parts.h"
+#include "lzw.h"
 #include <unordered_map>
 #include <iostream>
 #include <wolfssl/options.h>
---
+++ Server/lzw.h
@@ -0,0 +1,20 @@
+#ifndef __LZW_H__
+#define __LZW_H__
+
+
+#include <string>
+#include <array>
+
+// LZW
+/*
+    @ chunk_start： current chunk's start position
+    @ chunk_end:    current chunk's end position
+    @ s1 : chunk in string type
+    @ packet_size: packet size
+    @ output_code: output
+*/
+
+// void LZW(int chunk_start,int chunk_end,std::string &s_packet,unsigned int packet_size,std::vector<unsigned char> &output_code);
+void LZW(int chunk_start,int chunk_end,std::string &s1,unsigned int packet_size,unsigned char*output_code,size_t * outlen);
+
+#endif
