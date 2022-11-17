9399264e88e04b6cbb473ffc0a465cd6ab2a9795
PZZ97 <13573227027@163.com>
2022-11-17 11:59:12 -0500

    Update encode_parts.cpp

--- Server/encode_parts.cpp
+++ Server/encode_parts.cpp
@@ -11,62 +11,77 @@ using namespace std;
 #define MODULUS 256
 #define TARGET 0
 
-uint64_t hash_func(unsigned char* input, unsigned int pos)
-{
-	uint64_t hash = 0;
-	for (int i = 0; i < WIN_SIZE; i++) {
-		hash += int(input[pos + WIN_SIZE - 1 - i]) * pow(PRIME, i + 1);
-	}
-	return hash;
-}
+// uint64_t hash_func(unsigned char* input, unsigned int pos)
+// {
+// 	uint64_t hash = 0;
+// 	for (int i = 0; i < WIN_SIZE; i++) {
+// 		hash += int(input[pos + WIN_SIZE - 1 - i]) * pow(PRIME, i + 1);
+// 	}
+// 	return hash;
+// }
 
-uint64_t hash_func2(unsigned char* input, unsigned int pos, uint64_t hash_res)
-{
-	return hash_res * PRIME - int(input[pos-1]) * pow(PRIME, WIN_SIZE + 1) + int(input[pos-1 + WIN_SIZE]) * PRIME;
-}
+// uint64_t hash_func2(unsigned char* input, unsigned int pos, uint64_t hash_res)
+// {
+// 	return hash_res * PRIME - int(input[pos-1]) * pow(PRIME, WIN_SIZE + 1) + int(input[pos-1 + WIN_SIZE]) * PRIME;
+// }
 
+// void cdc(unsigned char* buff, unsigned int buff_size, IDXQ& chunk_q)
+// {
+//     CHUNK_idx_t chunk_index=0; 
+// 	uint64_t hash = 0;
+// 	for (unsigned int i = WIN_SIZE; i < buff_size - WIN_SIZE; i++) {
+// 		if (i == WIN_SIZE) {
+// 			hash = hash_func(buff, i);
+// 		}
+// 		else {
+// 			hash = hash_func2(buff, i, hash);
+// 		}
+// 		if ((hash % MODULUS) == TARGET) {
+// 			chunk_q.push({chunk_index++,i-1});
+// 		}
+// 	}
+//     chunk_q.push({chunk_index++,buff_size-1});  // the last chunk
+// }
 void cdc(unsigned char* buff, unsigned int buff_size, IDXQ& chunk_q)
 {
     CHUNK_idx_t chunk_index=0; 
-	uint64_t hash = 0;
-	for (unsigned int i = WIN_SIZE; i < buff_size - WIN_SIZE; i++) {
-		if (i == WIN_SIZE) {
-			hash = hash_func(buff, i);
-		}
-		else {
-			hash = hash_func2(buff, i, hash);
-		}
-		if ((hash % MODULUS) == TARGET) {
-			chunk_q.push({chunk_index++,i-1});
-		}
-	}
+    uint64_t hash = hash_func(buff,WIN_SIZE);
+    if((hash%MODULUS)==TARGET){
+        chunk_q.push({chunk_index++,WIN_SIZE-1});
+        printf("%d ",WIN_SIZE);
+    }
+    for( unsigned int i=WIN_SIZE+1;i<buff_size-WIN_SIZE;i++){
+        hash= hash*PRIME-buff[i-1]*pow(PRIME,WIN_SIZE+1)+buff[i-1+WIN_SIZE]*PRIME;
+        //hash_prev=hash;
+        if((hash%MODULUS)==TARGET){
+            chunk_q.push({chunk_index++,i-1});
+            printf("%d ",i);
+        }
+    }
     chunk_q.push({chunk_index++,buff_size-1});  // the last chunk
-
-
 }
 
-// void SHA_HW(char* message, char*digest){
-//     // https://edstem.org/us/courses/27305/discussion/2053707
-//     char shaSum[HASH_SIZE];
-//     Sha sha;
-//     wc_InitSha(&sha);
-//     wc_ShaUpdate(&sha, (const unsigned char*)message, strlen(message)); 
-//     wc_ShaFinal(&sha, (unsigned char*)digest);
-
-// }
-    // SHA_HW(input_buf,chunk_start_pos,chunk_end_pos, &hash_value);
+/*
+void SHA_HW(char* message, char*digest){
+    // https://edstem.org/us/courses/27305/discussion/2053707
+    char shaSum[HASH_SIZE];
+    Sha sha;
+    wc_InitSha(&sha);
+    wc_ShaUpdate(&sha, (const unsigned char*)message, strlen(message)); 
+    wc_ShaFinal(&sha, (unsigned char*)digest);
+}
+*/
 void SHA_HW( uint8_t* message,CHUNK_pos_t  chunk_start,CHUNK_pos_t chunk_end,  HASH *digest_hash){
     // https://edstem.org/us/courses/27305/discussion/2053707
-
     char digest[HASH_SIZE];
     Sha sha;
     wc_InitSha(&sha);
     wc_ShaUpdate(&sha, message+chunk_start,chunk_end-chunk_start+1 ); 
     wc_ShaFinal(&sha, (unsigned char*)digest);
-    cout<<"digest=";
-    for(int i=0;i<HASH_SIZE;i++)
-        printf("%02x",digest[i]);
-    cout<<endl;
+    // cout<<"digest=";
+    // for(int i=0;i<HASH_SIZE;i++)
+    //     printf("%02x",digest[i]);
+    // cout<<endl;
     *digest_hash=digest;
     
 }
